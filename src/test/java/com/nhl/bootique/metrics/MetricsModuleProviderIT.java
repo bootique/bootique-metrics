5943e5dd8da17e8c4db638226b9c92c9b47baf8e
Andrus Adamchik <andrus@objectstyle.com>
2016-08-01 13:38:57 +0300

    Upgrade to Bootique 0.19 #4

--- src/test/java/com/nhl/bootique/metrics/MetricsModuleProviderIT.java
+++ src/test/java/com/nhl/bootique/metrics/MetricsModuleProviderIT.java
@@ -1,21 +1,12 @@
 package com.nhl.bootique.metrics;
 
-import static java.util.stream.Collectors.counting;
-import static org.junit.Assert.assertEquals;
-
-import java.util.ServiceLoader;
-import java.util.stream.StreamSupport;
-
+import com.nhl.bootique.test.junit.BQModuleProviderChecker;
 import org.junit.Test;
-
-import com.nhl.bootique.BQModuleProvider;
 
 public class MetricsModuleProviderIT {
 
 	@Test
 	public void testPresentInJar() {
-		long c = StreamSupport.stream(ServiceLoader.load(BQModuleProvider.class).spliterator(), false)
-				.filter(p -> p instanceof MetricsModuleProvider).collect(counting());
-		assertEquals("No provider found", 1, c);
+		BQModuleProviderChecker.testPresentInJar(MetricsModuleProvider.class);
 	}
 }
